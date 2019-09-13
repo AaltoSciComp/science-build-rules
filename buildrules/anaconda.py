42eb042ae3a3e216f23b4f298a1e0c48f95020c2
Simo Tuomisto <simo.tuomisto@aalto.fi>
2019-09-13 11:02:21 +0300

    buildrules/anaconda.py: Adding directory creation rules

--- buildrules/anaconda.py
+++ buildrules/anaconda.py
@@ -8,6 +8,7 @@ import shutil
 import logging
 from glob import glob
 import yaml
+import copy
 
 from buildrules.common.builder import Builder
 from buildrules.common.rule import PythonRule, SubprocessRule, LoggingRule
@@ -54,8 +55,15 @@ class AnacondaBuilder(Builder):
                         'type': 'object',
                         'properties': {
                             'name': {'type': 'string'},
+                            'module_name': {'type': 'string'},
+                            'module_version': {'type': 'string'},
                             'miniconda': {'type': 'boolean'},
                             'installer_version': {'type': 'string'},
+                            'python_version': {
+                                'type': 'integer',
+                                'minimum': 2,
+                                'maximum': 3,
+                            },
                             'pip_packages': {
                                 'default' : [],
                                 'type': 'array',
@@ -67,7 +75,7 @@ class AnacondaBuilder(Builder):
                                 'items': {'type': 'string'},
                             },
                         },
-                        'required': ['name', 'version'],
+                        'required': ['name'],
                     },
                 },
             },
@@ -77,6 +85,10 @@ class AnacondaBuilder(Builder):
     def __init__(self, conf_folder):
         self._conda_path = os.path.join(os.getcwd(), 'conda')
         super().__init__(conf_folder)
+        self._source_cache = self._get_path('source_cache')
+        self._build_stage = self._get_path('build_stage')
+        self._install_path = self._get_path('install_path')
+        self._module_path = self._get_path('module_path')
 
     def _get_path(self, path_name):
         path_config = {
@@ -91,24 +103,46 @@ class AnacondaBuilder(Builder):
     def _get_directory_creation_rules(self):
         rules = []
 
-        install_path = self._get_path('install_path')
-        build_stage = self._get_path('build_stage')
-
         rules.extend([
-            LoggingRule('Creating stage directory: %s' % build_stage),
-            LoggingRule('Creating installation directory: %s' % build_stage)
+            LoggingRule('Creating cache directory: %s' % self._source_cache),
+            PythonRule(self._makedirs, [self._source_cache, 0o755]),
+            LoggingRule('Creating stage directory: %s' % self._build_stage),
+            PythonRule(self._makedirs, [self._build_stage, 0o755]),
+            LoggingRule('Creating installation directory: %s' % self._install_path),
+            PythonRule(self._makedirs, [self._install_path, 0o755]),
+            LoggingRule('Creating module directory: %s' % self._module_path),
+            PythonRule(self._makedirs, [self._module_path, 0o755]),
         ])
 
         return rules
 
+    def _create_environment_config(self, environment_dict):
+        default_config = {
+            'miniconda': True,
+            'python_version': 3,
+            'installer_version': 'latest',
+            'pip_packages': [],
+            'conda_packages': [],
+        }
+
+        config = copy.deepcopy(default_config)
+        config.update(environment_dict)
+        if 'module_name' not in config:
+            config['module_name'] = config['name']
+        if 'module_version' not in config:
+            config['module_version'] = '{name}{python_version}-{installer_version}'.format(**config)
+
+        return config
+
+
     def _get_environment_install_rules(self):
         rules = []
 
-        install_tree = self._get_path('build_stage')
-        build_stage = self._get_path('build_stage')
-
-        for environments in self._confreader['build_config']['environments']:
-            pass
+        for environment in self._confreader['build_config']['environments']:
+            config = self._create_environment_config(environment)
+            rules.append(LoggingRule((
+                "Installing conda environment '{name}' with "
+                "module '{module_name}/{module_version}'").format(**config)))
         
         return rules
 
