b77c5d0dc7640af6c402a22f5113bee52fea68b5
Simo Tuomisto <simo.tuomisto@aalto.fi>
2020-02-19 15:17:13 +0200

    run-worker.sh: Added script for running worker. Fixed a lmod issue with ubuntu dockerfile

---
+++ buildrules/ci/run-worker.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -e
+
+DOCKER_SERVER=${DOCKER_SERVER:-"docker.io"}
+DOCKER_USER=${DOCKER_USER:-"aaltoscienceit"}
+
+for ARG in "$@"
+do
+case $ARG in
+  -U|--user)
+  DOCKER_USER=$2
+  shift
+  shift
+  ;;
+  -s|--server)
+  DOCKER_SERVER=$2
+  shift
+  shift
+  ;;
+esac
+done
+
+if [[ "$#" -lt 1 ]]; then
+    cat << EOF
+
+    usage: ./$(basename $0) [-s|--server SERVER] [-U|--user user] TARGET [CMD]...
+
+         TARGET              worker target
+         CMD                 Run these commands instead of an interactive bash
+         -s|--server SERVER  Docker registry for the image
+         -U|--user USER      User in Docker registry
+
+EOF
+    exit
+fi
+
+TARGET=$1
+
+echo 'Creating buildbot home to /tmp/buildbot/'$TARGET
+BUILDBOT_HOME=/tmp/buildbot/$TARGET
+mkdir -p ${BUILDBOT_HOME}
+
+CMDS="bash -l"
+if [[ "$#" -gt 1 ]]; then
+    CMDS="bash -l -c ${@:2}"
+fi
+docker run -e WORKER_UID=$(id -u) -e HOME=/home/buildbot -v ${BUILDBOT_HOME}:/home/buildbot:rw -it aaltoscienceit/scibuilder-worker:$TARGET $CMDS
