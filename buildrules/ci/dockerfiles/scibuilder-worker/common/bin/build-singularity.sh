e2c38fbc830f5c58f667ba8513c5c7be5db61d66
Simo Tuomisto <simo.tuomisto@aalto.fi>
2020-10-21 11:41:42 +0300

    scibuilder-worker: Fixed a problem in build scripts

--- buildrules/ci/dockerfiles/scibuilder-worker/common/bin/build-singularity.sh
+++ buildrules/ci/dockerfiles/scibuilder-worker/common/bin/build-singularity.sh
@@ -9,6 +9,8 @@ CONF_DIR=/build/$BUILDER/build/configs/$TARGET/$BUILDER
 
 [[ -f /usr/share/lmod/lmod/init/bash ]] && . /usr/share/lmod/lmod/init/bash
 
+[[ -f /build/spack/build/spack/share/spack/setup-env.sh ]] && . /build/spack/build/spack/share/spack/setup-env.sh
+
 set +u
 
 echo "Running the $BUILDER builder: python3 -u -m buildrules $BUILDER $COMMAND $CONF_DIR"
