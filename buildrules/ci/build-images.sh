19d6875e93d5b36a7eb440159a16e50a80f3cb22
Simo Tuomisto <simo.tuomisto@aalto.fi>
2021-09-17 16:01:58 +0300

    Updated fgci-centos7-slurm-21.08 image to have Mellanox ib stack and self-compiled slurm 21.08

--- buildrules/ci/build-images.sh
+++ buildrules/ci/build-images.sh
@@ -6,7 +6,6 @@ PUSH=1
 IGNORE_CACHE=''
 DOCKER_SERVER=${DOCKER_SERVER:-"docker.io"}
 DOCKER_USER=${DOCKER_USER:-"aaltoscienceit"}
-WORKERS="fgci-centos7 aalto-ubuntu1804 aalto-ubuntu2004"
 
 for ARG in "$@"
 do
@@ -37,30 +36,37 @@ case $ARG in
 esac
 done
 
+if [ -z "$WORKERS" ]; then
+  WORKERS="fgci-centos7 aalto-ubuntu1804 aalto-ubuntu2004"
+  BUILD_SERVERS=y
+fi
+
 which docker &> /dev/null || ( echo "'docker'-command was not found. You need to install docker. Quitting!" ; exit 1)
 which j2 &> /dev/null || ( echo "'j2'-command was not found. You need to install j2cli. Quitting!" ; exit 1)
 
 cd dockerfiles
 
-# Build scibuilder-master and scibuilder-nfs-server
-for TARGET in scibuilder-master scibuilder-nfs-server ; do
-  BUILD_RESULT=1
-  docker build $IGNORE_CACHE -t $DOCKER_SERVER/$DOCKER_USER/$TARGET:latest -f $TARGET/Dockerfile .
-  BUILD_RESULT=$?
-  if [ $BUILD_RESULT -eq 0 ]; then
-    echo 'Build of "'$TARGET'" was successful.'
-  else
-    echo 'Build of "'$TARGET'" failed.'
-    exit 1
-  fi
-  if [ $PUSH -eq 0 ]; then
+if [ ! -z "$BUILD_SERVERS" ]; then
+  # Build scibuilder-master and scibuilder-nfs-server
+  for TARGET in scibuilder-master scibuilder-nfs-server ; do
+    BUILD_RESULT=1
+    docker build $IGNORE_CACHE -t $DOCKER_SERVER/$DOCKER_USER/$TARGET:latest -f $TARGET/Dockerfile .
+    BUILD_RESULT=$?
     if [ $BUILD_RESULT -eq 0 ]; then
-      docker push $DOCKER_SERVER/$DOCKER_USER/$TARGET:latest
+      echo 'Build of "'$TARGET'" was successful.'
     else
-      echo 'Build for "'$TARGET'" failed, will not push.'
+      echo 'Build of "'$TARGET'" failed.'
+      exit 1
     fi
-  fi
-done
+    if [ $PUSH -eq 0 ]; then
+      if [ $BUILD_RESULT -eq 0 ]; then
+        docker push $DOCKER_SERVER/$DOCKER_USER/$TARGET:latest
+      else
+        echo 'Build for "'$TARGET'" failed, will not push.'
+      fi
+    fi
+  done
+fi
 
 # Build worker-images
 cd scibuilder-worker
